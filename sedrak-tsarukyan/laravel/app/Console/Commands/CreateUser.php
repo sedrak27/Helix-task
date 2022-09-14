3b9fa36a901f4f7c899e7aeac599c68e7a560c37
Sedrak <sedrak.tsarukyan99@gmail.com>
2022-09-14 23:53:14 +0400

    change data validation

--- sedrak-tsarukyan/laravel/app/Console/Commands/CreateUser.php
+++ sedrak-tsarukyan/laravel/app/Console/Commands/CreateUser.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\MailController;
 use App\Models\User;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use phpDocumentor\Reflection\Types\Collection;
 
 class CreateUser extends Command
 {
@@ -29,59 +31,30 @@ class CreateUser extends Command
      * @return int
      */
     public function handle(): void{
-        $name = $this->ask('Enter user full name');
-        $email = $this->ask('Enter user email');
-        $isEmail = preg_match("/[-0-9a-zA-Z.+_]+@[-0-9a-zA-Z.+_]+.[a-zA-Z]{2,4}/",$email);
-        $isTaken = (bool)User::query()->where('email','=', $email)->first();
+        $datas = [];
+        $datas['name'] = $this->ask('Enter user full name');
+        $datas['email'] = $this->ask('Enter user email');
+        $datas['password'] = $this->secret('Enter user password');
 
-        while(!$isEmail || $isTaken) {
-            if($isTaken) {
-                $this->error('The email has already been taken');
-                unset($isTaken);
-            }else if(!$isEmail) {
-                $this->error('Invalid email try again, ex. name.surname@example.com');
+        $validation = Validator::make($datas,[
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:8',
+        ]);
+
+        if($validation->fails()){
+            foreach ($validation->errors()->all() as $error){
+                $this->error($error);
+                $this->line(' ');
             }
-
-            $email = $this->ask('Enter user email');
-            $isEmail = preg_match("/[-0-9a-zA-Z.+_]+@[-0-9a-zA-Z.+_]+.[a-zA-Z]{2,4}/",$email);
-            $isTaken = (bool)User::query()->where('email','=', $email)->first();
-        }
-
-        $password = $this->secret('Enter user password');
-        $shortLength = strlen($password) < 8;
-        $containLetters = preg_match('/[a-z]/i', $password);
-        $containNumbers = preg_replace( '/[^0-9]/', '', $password);
-
-        while($shortLength || !$containLetters || !$containNumbers){
-            if($shortLength){
-                $this->error('The password must be at least 8 characters');
-            } else if(!$containNumbers){
-                $this->error('Password must contain numbers');
-            } else if(!$containLetters){
-                $this->error('Password must contain letters');
+        } else{
+            $result = User::create($datas);
+            if($result){
+                $this->info('User created successfully');
+                $this->line(' ');
+            } else{
+                $this->error('Something went wrong try again');
             }
-            $password = $this->secret('Enter user password');
-            $shortLength = strlen($password) < 8;
-            $containLetters = preg_match('/[a-z]/i', $password);
-            $containNumbers = preg_replace( '/[^0-9]/', '', $password);
-        }
-
-        $bar = $this->output->createProgressBar(100);
-
-        $user = new User();
-        $user->name = $name;
-        $user->email = $email;
-        $user->password = Hash::make($password);
-        $user->verification_code = sha1(time());
-        $res = $user->save();
-
-        $bar->finish();
-
-        if($res){
-            MailController::sendEmail(name:$user->name, email:$user->email,verificationCode: $user->verification_code);
-            $this->info(' User created successfully check email for verification');
-        }else{
-            $this->info(' Something went wrong try again');
         }
     }
 }
